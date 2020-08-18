52eacf38c280d81fe0e2ac835b6c4723262a7a64
DanielSoaresRocha <legoliga123@gmail.com>
2020-08-18 15:01:11 -0300

    Criando as rotas do jogo da velha

--- src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { JogoDaVelhaComponent } from './jogo-da-velha.component';
 import { JogoDaVelhaService } from './shared'
 
+import { JogoDaVelhaRoutingComponent } from './jogo-da-velha-routing.component'
+import { JogoDaVelhaRoutingModule } from './jogo-da-velha-routing.module'
+
 
 @NgModule({
-  declarations: [JogoDaVelhaComponent],
+  declarations: [
+    JogoDaVelhaComponent,
+    JogoDaVelhaRoutingComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    JogoDaVelhaRoutingModule
   ],
   providers: [
     JogoDaVelhaService
