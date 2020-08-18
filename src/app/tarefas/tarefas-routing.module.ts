af820a8a76557875de8bf9572bcf280a1c2b94b5
DanielSoaresRocha <legoliga123@gmail.com>
2020-08-18 14:45:19 -0300

    adicionando rotas no módulo de tarefas

--- src/app/tarefas/tarefas-routing.module.ts
+++ src/app/tarefas/tarefas-routing.module.ts
@@ -1,24 +1,39 @@
-import { Routes } from '@angular/router'
+import { Routes, RouterModule } from '@angular/router'
 
 import { ListarTarefasComponent } from './listar'
 import { CadastrarTarefaComponent } from './cadastrar'
 import { EditarTarefasComponent } from './editar'
 
+import { TarefasRoutingComponent } from './tarefas-routing.component'
+import { NgModule } from '@angular/core'
+
 export const TarefaRoutes: Routes = [
     {
         path: 'tarefas',
-        redirectTo: 'tarefas/listar'
-    },
-    {
-        path: 'tarefas/listar',
-        component: ListarTarefasComponent
-    },
-    {
-        path: 'tarefas/cadastrar',
-        component: CadastrarTarefaComponent
-    },
-    {
-        path: 'tarefas/editar/:id',
-        component: EditarTarefasComponent
+        component: TarefasRoutingComponent,
+        children: [
+            {
+                path: '',
+                component: ListarTarefasComponent
+            },
+            {
+                path: 'cadastrar',
+                component: CadastrarTarefaComponent
+            },
+            {
+                path: 'editar/:id',
+                component: EditarTarefasComponent
+            }
+        ]
     }
 ]
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(TarefaRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class TarefasRoutingModule { }
--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -11,17 +11,22 @@ import { ListarTarefasComponent } from './listar';
 import { CadastrarTarefaComponent } from './cadastrar';
 import { EditarTarefasComponent } from './editar';
 
+import { TarefasRoutingComponent } from './tarefas-routing.component'
+import { TarefasRoutingModule } from './tarefas-routing.module'
+
 @NgModule({
   declarations: [
     ListarTarefasComponent,
     CadastrarTarefaComponent,
     EditarTarefasComponent,
-    TarefaConcluidaDirective
+    TarefaConcluidaDirective,
+    TarefasRoutingComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    TarefasRoutingModule
   ],
   providers: [
     TarefaService
